53a92832e17c7963fd4f885d27efa59763671653
Andrea Jasper <andrea.nicole.jasper@gmail.com>
2021-05-04 15:50:12 +0000

    Automatically backed up by Learn

--- components/Row.js
+++ components/Row.js
@@ -5,5 +5,17 @@ import Cell from './Cell';
 
 export default class Row extends React.Component {
   render () {
+    return(
+      <tr className="row">
+        { this.props.map((value, i) =>
+          <Cell
+            key={i}
+            rcell={value}
+            onChange={this.props.onChange.bind(null, i)}
+            onFocus={this.props.onFocus.bind(null, i)}
+          />
+        )}
+      </tr>
+    )
   }
 }
--- components/Table.js
+++ components/Table.js
@@ -5,5 +5,19 @@ import Row from './Row';
 
 export default class Table extends React.Component {
   render () {
+    return (
+    <table className="table" onBlur={this.props.onBlur}>
+      <tbody>
+      { this.props.table.map((value, i) =>
+        <Row
+          key={i}
+          row={value}
+          onChange={ this.props.onChange.bind(null, i)}
+          onFocus={ this.props.onChange.bind(null, i)}
+        />
+      )}
+      </tbody>
+    </table>
+    )
   }
 }
